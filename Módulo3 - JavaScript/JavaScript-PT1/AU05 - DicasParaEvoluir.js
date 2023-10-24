1f59d27ccd69a9fbed9b8e1033235d465b4133ec
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2023-10-24 06:10:00 -0300

    Anotando os exercícios na aula-ducas para evoluir.

--- Módulo3 - JavaScript/JavaScript-PT1/AU05 - DicasParaEvoluir.js
+++ Módulo3 - JavaScript/JavaScript-PT1/AU05 - DicasParaEvoluir.js
@@ -6,4 +6,10 @@
 
     Para colocar como meta, todos os exercicíos do material de apoio que eu fizer.. estarei anotando neste arquivo.
 
+    Exercício 1 -> concluido
+    Exercício 2 -> concluido
+    Exercício 3 -> concluido
+    Exercício 4 -> concluido
+    Exercício 5 -> concluido
+
 */
