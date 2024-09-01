c5d929b895f6bea8ef625ccc79d53937f1c7e9a9
jeffersonxbenetti <jefferson.benetti@hotmail.com>
2024-09-01 17:14:03 -0300

    Exercício 1, javascript parte 2.

---
+++ Módulo4 - JavaScript/Exercicíos/Módulo-Two-javascript/1°Exercício.js
@@ -0,0 +1,6 @@
+// Escreva um programa que verifique se um número é par ou ímpar e exiba o resultado no console.
+
+
+for (let i = 0; i <= 30; i++) {
+    i % 2 === 0 ? console.log(`O número ${i} é PAR`) : console.log(`O número ${i} é IMPAR`)
+}
